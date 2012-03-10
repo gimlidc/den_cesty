f3292e3ff60a63a1567c6ca0470206e01d227d7a
root <root@localhost.localdomain>
2012-03-10 23:35:55 +0100

    Fixed production version
    	modified:   Gemfile.lock
    	modified:   Rakefile
    	modified:   config/environments/production.rb
    	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120129125737) do
+ActiveRecord::Schema.define(:version => 20120310091052) do
+
+  create_table "posts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "registrations", :force => true do |t|
     t.integer  "walker_id"
