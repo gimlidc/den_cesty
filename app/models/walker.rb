ec3559fb4a54fbc6993c459638b19457b1fcbfac
gimli <gimli@matfyz.cz>
2012-03-11 23:24:46 +0100

    Confirmation added
    	modified:   app/models/walker.rb

--- app/models/walker.rb
+++ app/models/walker.rb
@@ -1,7 +1,7 @@
 class Walker < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, # :confirmable,
+  devise :database_authenticatable, :registerable, :confirmable,
 		:recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
 
 	has_many :registration
