25002ccff0e0a7608087abf793b3b76aa3164370
root <root@localhost.localdomain>
2012-03-11 23:26:53 +0100

    Added production mailer
    	modified:   config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,8 @@ DenCesty::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => '77.93.202.133:3000' }
 
   # Enable threaded mode
   # config.threadsafe!
