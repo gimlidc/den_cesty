25c742e214a160d45ab9079d87e78842d0ca8078
gimli <gimli@matfyz.cz>
2012-03-19 09:13:33 +0100

    Distance editation added.
    	modified:   app/controllers/admin_controller.rb
    	modified:   app/views/admin/results_setting.html.erb
    	modified:   config/locales/cs.yml
    	modified:   config/locales/en.yml
    	modified:   config/navigation.rb
    	modified:   config/routes.rb

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,16 +2,48 @@ class AdminController < ApplicationController
 
 	def results_update
 		if walker_signed_in? && current_walker.username == $admin_name
+			dc_id = Integer("#{params[:dc_id]}")
+			@walkers = Walker.find(:all, :order => "id");
+			@results = Result.where(:dc_id => dc_id).order(:walker_id)
+			res = 0
+			@walkers.each do |walker|
+				if @results.nil? || @results.empty? || @results[res].nil? || @results[res].walker_id != walker.id
+					result = Result.new
+					result.dc_id = dc_id
+					result.walker_id = walker.id
+				else
+					result = @results[res]
+					res += 1
+				end
+
+				distance = params["#{walker.id}"]
+				if distance != ""
+					result.distance = distance
+
+					if (!result.save)
+						flash[:notice] = "#{flash[:notice]}, result: #{result.walker_id},#{result.dc_id},#{result.distance} not saved";
+					else
+						flash[:notice] = "#{flash[:notice]}, result: #{result.walker_id},#{result.dc_id},#{result.distance} saved";
+					end
+				else
+					flash[:notice] = "#{flash[:notice]}, distance for walker: #{walker.id} not set";
+				end
+			end
+			redirect_to :action => "results_setting", :id => dc_id
 		else
-			rediredct_to :action => 'unauthorized'
+			redirect_to :action => 'unauthorized'
 		end
 	end
 
 	def results_setting
 		if walker_signed_in? && current_walker.username == $admin_name
+			if params[:id].nil?
+				@set_dc = $current_dc_id
+			else
+				@set_dc = Integer("#{params[:id]}")
+			end
 			@walkers = Walker.find(:all, :order => "surname");
-			@results = Result.find(:all, :order => "walker_id, dc_id")
-			@set_dc = Integer("#{params[:id]}")
+			@results = Result.joins(:walker).order('walkers.surname', 'dc_id')
 		else
 			rediredct_to :action => 'unauthorized'
 		end
