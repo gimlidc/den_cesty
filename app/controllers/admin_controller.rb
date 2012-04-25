847e89002c27a1d3d9c58f2bc33b2e0a8e6d54e5
gimli <gimli@matfyz.cz>
2012-04-25 16:45:27 +0200

    Actualized pages after DC
    	modified:   app/controllers/admin_controller.rb
    	new file:   app/views/admin/merge_list.html.erb
    	modified:   app/views/pages/actual.html.erb
    	renamed:    app/views/pages/actual.html.erb -> app/views/pages/jaro2012.html.erb

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,92 @@
 class AdminController < ApplicationController
 
+	def merge
+		@walker_a = Walker.find(:first, :conditions => { :id => params[:walkerA]})
+		@walker_b = Walker.find(:first, :conditions => { :id => params[:walkerB]})
+		if !@walker_a.nil? && !@walker_b.nil?
+			@results_a = Result.where(:walker_id => @walker_a.id).order(:dc_id)
+			@results_b = Result.where(:walker_id => @walker_b.id).order(:dc_id)
+
+			# check problems in results
+			a=0
+			b=0
+			for i in 1..$current_dc_id
+				while(!@results_a[a].nil? && @results_a[a].dc_id < i)
+					a += 1
+				end
+				while(!@results_b[b].nil? && @results_b[b].dc_id < i)
+					b += 1
+				end
+
+				if (!@results_b[b].nil? && @results_a[a] == @results_b[b])
+					flash[:alert] = "Conflict in results: #{@results_a[a]}, double record! Merge failed"
+					redirect_to :action => 'merge_list'
+					return
+				end
+			end
+
+			@reports_a = Report.where(:walker_id => @walker_a.id).order(:dc_id)
+			@reports_b = Report.where(:walker_id => @walker_b.id).order(:dc_id)
+
+			# check problems in reports
+			a=0
+			b=0
+			for i in 1..$current_dc_id
+				while(!@reports_a[a].nil? && @reports_a[a].dc_id < i)
+					a += 1
+				end
+				while(!@reports_b[b].nil? && @reports_b[b].dc_id < i)
+					b += 1
+				end
+
+				if (!@reports_a[a].nil? && @reports_a[a] == @reports_b[b])
+					flash[:alert] = "Conflict in reports: #{@reports_a[a]}, double record! Merge failed"
+					redirect_to :action => 'merge_list'
+					return
+				end
+			end
+
+			#everything seems to be OK
+			if !@results_a.nil? && !@results_a.empty?
+				@results_a.each do |result|
+					result.walker_id = @walker_b.id
+					if !result.save
+						flash[:alert] = "Result #{result.id} rewrite walker_id failed. Please do it manually\n"
+					end
+				end
+			end
+
+			if !@reports_a.nil? && !@reports_a.empty?
+				@reports_a.each do |report|
+					report.walker_id = @walker_b.id
+					if !result.save
+						flash[:alert] = "Result #{result.id} rewrite walker_id failed. Please do it manually\n"
+					end
+				end
+			end
+
+			if !@walker_a.delete
+				flash[:alert] = "Merged walker #{@walker_b.id} cannot be deleted. Do it manually.\n"
+			else
+				flash[:notice] = "Merge successful\n"
+			end
+		else
+			flash[:alert] = "Walkers not found"
+		end
+		redirect_to :action => 'merge_list'
+	end
+
+	def merge_list
+		@walkers = Walker.find(:all, :order => "surname, name")
+		@options = ""
+		@walkers.each do |walker|
+			@options += "<option value=\"#{walker.id}\">#{walker.surname} #{walker.name}, #{walker.year}</option>\n"
+		end
+	end
+
 	def print_list
 		if (walker_signed_in? && current_walker.username == $admin_name)
-			@registration = Registration.joins(:walker).where(:canceled => false)
+			@registration = Registration.joins(:walker).where(:canceled => false).order(:surname)
 		end
 	end
 
@@ -48,7 +132,7 @@ class AdminController < ApplicationController
 			else
 				@set_dc = Integer("#{params[:id]}")
 			end
-			@walkers = Walker.find_by_sql("SELECT wal_reg.id AS id, name, surname, year, wal_reg.dc_id AS dc_id, distance FROM (SELECT walkers.id AS id, name, surname, year, registrations.dc_id AS dc_id FROM walkers JOIN registrations ON walkers.id = registrations.walker_id) AS wal_reg LEFT OUTER JOIN results ON wal_reg.id = results.walker_id AND wal_reg.dc_id = results.dc_id")
+			@walkers = Walker.find_by_sql("SELECT wal_reg.id AS id, name, surname, year, wal_reg.dc_id AS dc_id, distance FROM (SELECT walkers.id AS id, name, surname, year, registrations.dc_id AS dc_id FROM walkers JOIN registrations ON walkers.id = registrations.walker_id WHERE registrations.dc_id = #{@set_dc}) AS wal_reg LEFT OUTER JOIN results ON wal_reg.id = results.walker_id AND wal_reg.dc_id = results.dc_id")
 		else
 			rediredct_to :action => 'unauthorized'
 		end
