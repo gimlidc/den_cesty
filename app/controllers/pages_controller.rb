42c6502c9dfb128a91ff76ed384e8bab7a41b69f
gimli <gimli@matfyz.cz>
2012-08-16 15:06:06 +0200

    Autumn den cesty, registration, dates.
    	modified:   app/controllers/application_controller.rb
    	modified:   app/controllers/pages_controller.rb
    	modified:   app/views/pages/actual.html.erb
    	renamed:    app/views/pages/actual.html.erb -> app/views/pages/jar12_16.html.erb
    	new file:   app/views/pages/podzim2012.html.erb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def actual
 		@registered_walkers = Registration.where(:dc_id => $current_dc_id, :canceled => false).joins(:walker).order(:username)
+		render "podzim2012.html.erb"
   end
 
 	def rules
