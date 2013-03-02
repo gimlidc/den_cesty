3e9ebb39a71463b3b3deaddefa0bcd62ab1210a9
Honza Blažek <gimli@matfyz.cz>
2013-03-03 00:25:37 +0100

    fixed layout plus fixed report list

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -6,29 +6,31 @@ class ReportController < ApplicationController
 	def list
 	  # 
 		@dc_id = (params[:id].nil? || params[:id] == "") ? nil : params[:id]
-		@author = (params[:author].nil? || params[:author] == "") ? nil : Walker.find(:all, :conditions => { :id => params[:author] })[0]
+		@author = (params[:walker].nil? || params[:walker] == "") ? nil :  Walker.where(:id => params[:walker]).first
 
     # user does not select nothing
 		if (@dc_id == nil && @author == nil)
 			@reports = Report.joins(:walker).find(:all)
-			@walkers = Report.joins(:walker).select("username, walker_id").uniq
+			publishers = Report.select("walker_id")
+			@walkers = Walker.where(:id => publishers).select(["username", "id"])
 		else
 		  # user selects Den Cesty
 			if (@author == nil)
 				@dc_id = params[:id]
 				@reports = Report.joins(:walker).find(:all, :conditions => {:dc_id => @dc_id})
-				@walkers = Report.joins(:walker).where(:dc_id => @dc_id).select("username, walker_id").uniq
+				publishers = Report.where(:dc_id => @dc_id).select("walker_id")
+        @walkers = Walker.where(:id => publishers).select(["username", "id"])
 			# user selects Author
 			else
 			  # Den Cesty is also defined
 				if (@dc_id == nil)
-					@reports = Report.joins(:walker).find(:all, :conditions => {:walker_id => params[:author]})
-					@walkers = Report.joins(:walker).select("username, walker_id").uniq
+					@reports = Report.joins(:walker).find(:all, :conditions => {:walker_id => params[:walker]})
+					@walkers = Walker.where(:id => @author).select(["username", "id"])
 				# wtf?
 				else
 					@dc_id = params[:id]
-					@reports = Report.joins(:walker).find(:all, :conditions => {:dc_id => @dc_id, :walker_id => params[:author]})
-					@walkers = Report.joins(:walker).where(:dc_id => @dc_id).select("username, walker_id").uniq
+					@reports = Report.joins(:walker).find(:all, :conditions => {:dc_id => @dc_id, :walker_id => params[:walker]})
+					@walkers = Walker.where(:id => @author).select(["username", "id"])
 				end
 			end
 		end
