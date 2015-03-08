b002e927d9eecbda27929a396d8616e7868d6299
machl <putas.m@seznam.cz>
2015-03-08 18:26:03 +0100

    Added Races controller and basic Scoreboard list

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -3,14 +3,12 @@ class ScoreboardController < ApplicationController
   skip_before_filter :check_admin?
   skip_before_filter :check_logged_in?
 
-  layout false
-
-  # Web methods:
 
   # Shows race progress and walkers order based on elapsed distance.
-  # /scoreboard
+  # /scoreboard/id
   def index
-    @scoreboard = Scoreboard.where(:dc_id => $dc.id).order("distance DESC")
+    @race = Race.find(params[:id])
+    @scoreboard = @race.scoreboard.order("distance DESC")
   end
 
 end
