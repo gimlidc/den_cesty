e62f08f13338a39aafb811636eb9251a4ea5e839
Honza Blazek <gimli@matfyz.cz>
2016-10-06 14:52:24 +0200

    added qrcode generator into registration update

--- app/mailers/walker_mailer.rb
+++ app/mailers/walker_mailer.rb
@@ -8,46 +8,49 @@ class WalkerMailer < ActionMailer::Base
   def send_payment_details(walker)
     
   end
-  
-  def send_payment_request(registration)
-    @walker = registration.walker
-    @reg = registration
 
+  def createQrCode(reg)
     qrstring = "SPD*1.0*ACC:" << $IBAN
-    qrstring = qrstring << "*AM:" << price(@reg).to_s << "*CC:CZK"
-    qrstring = qrstring << "*X-SS:666" << "*X-VS:" << sprintf("%03d", $dc.id) << sprintf("%04d",@reg.walker_id)
-    qrstring = qrstring << "*MSG:" << @reg.walker[:email]
+    qrstring = qrstring << "*AM:" << price(reg).to_s << "*CC:CZK"
+    qrstring = qrstring << "*X-SS:666" << "*X-VS:" << sprintf("%03d", $dc.id) << sprintf("%04d",reg.walker_id)
+    qrstring = qrstring << "*MSG:" << reg.walker[:email]
 
     qrcode = RQRCode::QRCode.new(qrstring)
     # With default options specified explicitly
-    @svg = qrcode.as_svg(offset: 0, color: '000',
+    return qrcode.as_svg(offset: 0, color: '000',
                          shape_rendering: 'crispEdges',
                          module_size: 4)
+  end
 
+  def send_payment_request(registration)
+    @walker = registration.walker
+    @reg = registration
+    @svg = createQrCode(@reg)
     mail(to: registration.walker.email, subject: I18n.t("registration payment notification"))
   end
   
   def notify_registration_update(registration)
     @walker = registration.walker
     @reg = registration
+    @svg = createQrCode(@reg)
     mail(to: registration.walker.email, subject: I18n.t("registration updated"))
   end
   
   def notify_registration_transfer(walkerFrom, walkerTo)
-    notify_registration_removed(walkerFrom)
-    notify_registration_added(walkerTo)
+    notify_registration_removed(walkerFrom, walkerTo)
+    notify_registration_added(walkerTo, walkerTo)
   end
   
   def notify_registration_removed(walkerFrom, walkerTo)
     @walker = walkerFrom
     @newOwner = walkerTo
-    mail(to: walker.email, subject: I18n.t("registration changed owner"))
+    mail(to: @walker.email, subject: I18n.t("registration changed owner"))
   end
   
   def notify_registration_added(walkerFrom, walkerTo)
     @walker = walkerTo
     @oldOwner = walkerFrom
-    mail(to: walker.email, subject: I18n.t("registration changed owner"))
+    mail(to: @walker.email, subject: I18n.t("registration changed owner"))
   end
   
   def send_spam(walker, subj, message_content)
