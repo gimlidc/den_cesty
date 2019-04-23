6377c7a27f1f7a2b011c78c3fc9b6e33ddc70112
evajs <evajs@matfyz.cz>
2019-04-23 21:58:37 +0200

    registrace test deadline v menu a testech

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,6 +15,7 @@ class ActiveSupport::TestCase
     $dc = dcs(:two)
     $shirt_deadline = ($dc.start_time - 17.days).end_of_day
     $registration_deadline = ($dc.start_time - 4.days).end_of_day
+    $registration_edit_deadline = $dc.start_time - 4.hours
     $registration_starts = true
     $report_deadline = ($dc.start_time + 1.month).end_of_day
   end
