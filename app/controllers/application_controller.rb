931caca6c6a4538c9d5e6e2b9129d6026f4c3dc6
Honza Blažek <gimli@matfyz.cz>
2020-02-10 13:49:33 +0100

    added dc 33

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -75,7 +75,7 @@ class ApplicationController < ActionController::Base
 
   def loadDcParams
     if ENV["RAILS_ENV"] != "test"
-      $dc = Dc.find(32)
+      $dc = Dc.find(33)
       $dc_app_start = $dc.start_time - 10.minutes
       $dc_app_end =   $dc.start_time + 12.hours + 10.minutes
       $shirt_deadline = ($dc.start_time - 17.days).end_of_day
